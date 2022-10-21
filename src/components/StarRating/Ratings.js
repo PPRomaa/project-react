3c17c35d8411f2a04ddb1fb0ddc4401c62551357
PPRomaa <popadinets.roman1@gmail.com>
2022-10-21 16:54:47 +0300

    movie-project

---
+++ src/components/StarRating/Ratings.js
@@ -0,0 +1,21 @@
+import StarRatings from "react-star-ratings/build/star-ratings";
+import css from '../MoviesList/movieList.module.css'
+
+const Rating = ({rating}) => {
+    return (
+        <div className={css.stars}>
+            <StarRatings
+                classname={'a'}
+                rating={rating}
+                starRatedColor="yellow"
+                numberOfStars={10}
+                name='rating'
+                starDimension="20px"
+                starSpacing="3px"
+            />
+        </div>
+
+    );
+};
+
+export {Rating};
