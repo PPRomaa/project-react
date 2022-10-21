02fd3cb8293cc2049424ad06b80d8734c9540f7c
PPRomaa <popadinets.roman1@gmail.com>
2022-10-21 15:19:06 +0300

    movie-project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,33 @@ import {NavLink} from "react-router-dom";
 import {useColorMode} from "theme-ui";
 
 import css from './header.module.css'
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {movieActions} from "../../redux";
+import {UserInfo} from "../UserInfo/UserInfo";
 
 const Header = () => {
     const [colorMode,setColorMode] = useColorMode();
+
+    const [query,setQuery] = useState('');
+    const dispatch = useDispatch();
+
+    const search = (e) => {
+        e.preventDefault()
+        if (query){
+            dispatch(movieActions.searchMovie(query))
+        }else dispatch(movieActions.getAllMovies())
+    }
+    const find = (e) => {
+        e.preventDefault()
+        if (query){
+            dispatch(movieActions.searchMovie(query))
+        }
+        setQuery('')
+    }
+
     return (
         <div className={css.headerBlock}>
-
             {/*<div>*/}
             {/*    <img src="/" alt="photo"/>*/}
             {/*    <NavLink to={'/'}>All movies</NavLink>*/}
@@ -20,8 +41,9 @@ const Header = () => {
             <div>
                 <span>burger</span>
             </div>
-            <form>
-                <input type="text" placeholder={'find movie'}/>
+            <form onChange={search}>
+                <input type="text" placeholder={'find movie'} onChange={(e)=>setQuery(e.target.value)} value={query}/>
+                <button onClick={find}>Find</button>
             </form>
             <div>
                 <label>
@@ -31,9 +53,7 @@ const Header = () => {
                     <span></span>
                 </label>
             </div>
-            <div>
-                <h3>Name</h3>
-            </div>
+            <UserInfo/>
         </div>
     );
 };
---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,9 @@
+const UserInfo = () => {
+  return(
+      <div>
+          <div>Hello</div>
+          <div>Name</div>
+      </div>
+  )
+}
+export {UserInfo}
